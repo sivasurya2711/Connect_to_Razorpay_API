5bb36cdb54918643acc5ca502e4e7789468ca362
sivasurya2711 <siva.surya@goevive.com>
2021-01-27 16:54:21 +0530

    Create orders_api.py

---
+++ orders_api.py
@@ -0,0 +1,15 @@
+import requests
+import json
+import razorpay
+from requests.auth import HTTPBasicAuth
+data = {
+    "amount" : 50000,
+    "currency": "INR",
+    "receipt" :"dummy_order_id_1"
+}
+response = requests.post('https://api.razorpay.com/v1/orders',
+                        auth=HTTPBasicAuth('rzp_test_9vEgMk4HDRYKQR', 'jUcGgCuR0EbAWCPBpbhu1SyQ'),
+                         data=data)
+with open("response.py", "w") as temp:
+    json.dump(response.json(), temp, indent=4)
+
